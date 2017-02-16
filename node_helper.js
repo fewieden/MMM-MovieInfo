8d87b6ed739553cf65fde0593fd8f79612ed12c9
dr4ke616 <adamdrakeford@gmail.com>
2017-02-16 23:00:57 +0000

    Implemented configruation based queries

--- node_helper.js
+++ node_helper.js
@@ -9,11 +9,19 @@
 const request = require('request');
 const moment = require('moment');
 const NodeHelper = require('node_helper');
+require('datejs');
 
 module.exports = NodeHelper.create({
 
     start: function() {
         console.log("Starting module: " + this.name);
+    },
+
+    serialize: function(obj) {
+        return '?'+Object.keys(obj).reduce(function(a, k) {
+            a.push(k + '=' + encodeURIComponent(obj[k]));
+            return a
+        },[]).join('&')
     },
 
     socketNotificationReceived: function(notification, payload) {
@@ -31,16 +39,21 @@ module.exports = NodeHelper.create({
 	 * Request data from the supplied URL and broadcast it to the MagicMirror module if it's received.
 	 */
     getData: function() {
-        var start = moment().format("YYYY-MM-DD");
-        var end = moment().add(1, "months").format("YYYY-MM-DD");
+        var discover = this.config.discover;
+        discover['api_key'] = this.config.api_key
+        discover['language'] = (this.config.language ? this.config.language : 'en')
 
-		var options = {
-			url: 'https://api.themoviedb.org/3/discover/movie?' +
-                'primary_release_date.gte=' + start +
-                '&primary_release_date.lte=' + end +
-                '&api_key=' + this.config.api_key +
-                '&language=' + (this.config.language ? this.config.language : 'en')
-		};
+        if ('primary_release_date.gte' in discover)
+            discover['primary_release_date.gte'] = Date.parse(discover['primary_release_date.gte']).toString("yyyy-MM-d");
+
+        if ('primary_release_date.lte' in discover)
+            discover['primary_release_date.lte'] = Date.parse(discover['primary_release_date.lte']).toString("yyyy-MM-d");
+
+        var query = this.serialize(discover);
+        var options = {
+            url: 'https://api.themoviedb.org/3/discover/movie' + this.serialize(discover)
+        };
+
         request(options, (error, response, body) => {
             if (response.statusCode === 200) {
                 this.sendSocketNotification("DATA", JSON.parse(body));
